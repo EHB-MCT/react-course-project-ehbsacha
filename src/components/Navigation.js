1b7c8b817d068fd91061eb73f0ea92592b6e4eb8
ehbsacha <sacha.voskuilen@student.ehb.be>
2022-05-13 13:06:42 +0200

    Updated url

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,13 +2,13 @@ function Navigation(props) {
   return (
     <nav className="navigation">
       <div className="home">
-        <a href="http://localhost:4200/"><img src={props.logo} alt="Lekker van bij ons logo" /></a>
+        <a href="https://glowing-salmiakki-6df887.netlify.app/"><img src={props.logo} alt="Lekker van bij ons logo" /></a>
       </div>
       <div className="navButtons">
         <a className="navItem">Zoeken <i className="fas fa-search"></i></a>
         <a className="navItem">Gerechten <i className="fas fa-hamburger"></i></a>
         <a className="navItem">Favorieten <i className="fas fa-heart"></i></a>
-        <a href="http://localhost:4200/profile" className="navItem">Profiel<i className="fas fa-user-circle"></i></a>
+        <a href="https://glowing-salmiakki-6df887.netlify.app/profile" className="navItem">Profiel<i className="fas fa-user-circle"></i></a>
       </div>
     </nav>
   );
