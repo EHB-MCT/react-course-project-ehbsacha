f7325ff2d86561746f5f3f6c0702b33630f09891
ehbsacha <71716345+ehbsacha@users.noreply.github.com>
2022-08-12 22:49:23 +0200

    Fetch happens back in the service

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -2,30 +2,9 @@ import backgroundImage from '../img/backgroundImage.jpg';
 import PrewiewBlock from './PreviewBlock';
 import FilterSystem from './FilterSystem';
 import React, { useState, useEffect } from 'react';
+import { recipeService } from '../services/apiService';
 
 function Filter() {
-  // ApiKeys, each of them can do a certain amount of fetchcalls per day.
-  // 150 points per key but it is a bit less than that in calls
-  // 9d27c0e3e70247c2a0b1861d56e60375
-  // a63bbb37b3b84baba1de52681e102f7e
-  // 720fb95fb7e449d5be4fb2a453ce7db4
-  // 0d9cf745b2b14fb0b1ec483abf3800cc
-  // c4f7d95969c34c009df7bfc1b3f31347
-  // fd29be03ea994da0a1d828da3d843a44
-  // 66cb98c71b8e4c1e8bb94007b62a14c5
-  // 3cea7defa0894eb69af1b16ead313107
-  // a5a02c1e5a15414092f974b1aff77a10
-  // 1e8cf27f56954a8095c6c173cfcc52e6
-
-  // password
-
-  // Constant values
-  const baseUrl = 'https://api.spoonacular.com/'
-  const typeOfRoute = 'recipes/complexSearch?';
-  const returnAllRecipeInformation = true
-  const limit = 9;
-  const apiKey = '1e8cf27f56954a8095c6c173cfcc52e6'
-  const endUrl = `&number=${limit}&addRecipeInformation=${returnAllRecipeInformation}&apiKey=${apiKey}`
 
   // recipes are stored here
   const [recipes, setRecipes] = useState([]);
@@ -39,15 +18,13 @@ function Filter() {
   // fullUrl items get updated
   var extraVariables = `query=${query.toString()}&intolerances=${intolerances.toString()}&type=${typeFilter}`;
 
-  var fullUrl = `${baseUrl}${typeOfRoute}${extraVariables}${endUrl}`;
-
   // Previous api
   // https://recepten-liese-c.herokuapp.com/recipes
 
   useEffect(() => {
-    fetch(`${fullUrl}`)
-      .then((response) => response.json())
+    recipeService.complexSearchFetch(extraVariables)
       .then((data) => setRecipes(data.results))
+      .then(console.log(recipes))
   }, []);
 
   useEffect(() => {
@@ -56,10 +33,10 @@ function Filter() {
   }, [typeFilter, intolerances])
 
   function updateFetch() {
-    setRecipes([]);
-    fetch(fullUrl)
-      .then((response) => response.json())
+    // setRecipes([]);
+    recipeService.complexSearchFetch(extraVariables)
       .then((data) => setRecipes(data.results))
+      .then(console.log(recipes))
   }
 
   return (
--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,15 +1,43 @@
-class fetch {
+class RecipeService {
+    // baseUrl = "https://recepten-liese-c.herokuapp.com/"; <- previous api
 
-    baseUrl = "https://recepten-liese-c.herokuapp.com/";
+    // basic required stuff
+    baseUrl = `https://api.spoonacular.com/`;
+    key = `98fe6b3b568e43a1abfa764bed5db7f8`;
+    apiKey = `&apiKey=${this.key}`;
 
-    async getAll() {
-        return (await fetch(this.baseUrl + "recipes")).json();
+    // type of search
+    complexSearch = 'recipes/complexSearch?';
+
+    // Extra request items
+    returnAllRecipeInformation = true;
+    limit = 15;
+
+    // Somewhat reusable url end string
+    endUrl = `&number=${this.limit}&addRecipeInformation=${this.returnAllRecipeInformation}${this.apiKey}`
+
+    /*
+    // Fetch functions
+    */
+    async complexSearchFetch(query) {
+        const response = await fetch(`${this.baseUrl}/${this.complexSearch}${query}${this.endUrl}`);
+        return response.json();
     }
-
-    // async getId(id) {
-    //     return (await fetch(this.baseUrl + `recipes/${id}`)).json();
-    // }
 
 }
 
-export const fetchRecipes = new fetch();
+// ApiKeys, each of them can do a certain amount of fetchcalls per day.
+// 150 points per key but it is a bit less than that in calls
+// 9d27c0e3e70247c2a0b1861d56e60375
+// a63bbb37b3b84baba1de52681e102f7e
+// 720fb95fb7e449d5be4fb2a453ce7db4
+// 0d9cf745b2b14fb0b1ec483abf3800cc
+// c4f7d95969c34c009df7bfc1b3f31347
+// fd29be03ea994da0a1d828da3d843a44
+// 66cb98c71b8e4c1e8bb94007b62a14c5
+// 3cea7defa0894eb69af1b16ead313107
+// a5a02c1e5a15414092f974b1aff77a10
+// 1e8cf27f56954a8095c6c173cfcc52e6
+// 98fe6b3b568e43a1abfa764bed5db7f8
+
+export const recipeService = new RecipeService();
